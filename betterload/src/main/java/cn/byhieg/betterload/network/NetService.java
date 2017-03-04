686dff5d9260def4e58f47094d03eaca818a7e34
石琦峰 <byhieg@gmail.com>
2017-03-04 22:27:58 +0800

    封装了retrofit

--- betterload/src/main/java/cn/byhieg/betterload/network/NetService.java
+++ betterload/src/main/java/cn/byhieg/betterload/network/NetService.java
@@ -1,5 +1,6 @@
 package cn.byhieg.betterload.network;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import cn.byhieg.betterload.utils.FailureMessage;
@@ -22,19 +23,20 @@ public class NetService {
     private OkHttpClient okHttpClient;
     private Retrofit retrofit;
     private HttpLoggingInterceptor loggingInterceptor;
-    private NetService(){
+
+    private NetService() {
         loggingInterceptor = new HttpLoggingInterceptor();
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
     }
 
     private static NetService netService = new NetService();
 
-    public static NetService getInstance(){
+    public static NetService getInstance() {
         return netService;
     }
 
 
-    public NetService init(String baseUrl){
+    public NetService init(String baseUrl) {
         synchronized (this) {
             okHttpClient = new OkHttpClient.Builder().
                     readTimeout(15, TimeUnit.SECONDS).
@@ -59,7 +61,7 @@ public class NetService {
     }
 
 
-    public <T> void asynRequest(final Call<T> requestCall , final IResonseListener<T> resonseListener){
+    public <T> void asynRequest(final Call<T> requestCall, final IResonseListener<T> resonseListener) {
 
         final FailureMessage failureMessage = new FailureMessage();
         if (resonseListener == null) {
@@ -74,40 +76,86 @@ public class NetService {
         }
 
 
-       call.enqueue(new Callback<T>() {
-           @Override
-           public void onResponse(Call<T> call, Response<T> response) {
-               int resultCode = response.code();
-               if (response.isSuccessful()) {
-                   T result = response.body();
-                   if (result == null) {
-                       failureMessage.clear();
-                       failureMessage.setResultCode(resultCode);
-                       failureMessage.setFailureMessage("body为空");
-                       resonseListener.onFailure(failureMessage.toString());
-                   }else{
-                       resonseListener.onSucess(result);
-                   }
+        call.enqueue(new Callback<T>() {
+            @Override
+            public void onResponse(Call<T> call, Response<T> response) {
+                int resultCode = response.code();
+                if (response.isSuccessful()) {
+                    T result = response.body();
+                    if (result == null) {
+                        failureMessage.clear();
+                        failureMessage.setResultCode(resultCode);
+                        failureMessage.setFailureMessage("body为空");
+                        resonseListener.onFailure(failureMessage.toString());
+                    } else {
+                        resonseListener.onSucess(result);
+                    }
 
-               }else{
-                   failureMessage.clear();
-                   failureMessage.setResultCode(resultCode);
-                   failureMessage.setFailureMessage(resultCode + "错误");
-                   resonseListener.onFailure(failureMessage.toString());
+                } else {
+                    failureMessage.clear();
+                    failureMessage.setResultCode(resultCode);
+                    failureMessage.setFailureMessage(resultCode + "错误");
+                    resonseListener.onFailure(failureMessage.toString());
 
-               }
+                }
 
 
-           }
+            }
 
-           @Override
-           public void onFailure(Call<T> call, Throwable t) {
-               failureMessage.clear();
-               failureMessage.setResultCode(-1);
-               failureMessage.setFailureMessage(t.getMessage());
-               resonseListener.onFailure(failureMessage.toString());
-           }
-       });
+            @Override
+            public void onFailure(Call<T> call, Throwable t) {
+                failureMessage.clear();
+                failureMessage.setResultCode(-1);
+                failureMessage.setFailureMessage(t.getMessage());
+                resonseListener.onFailure(failureMessage.toString());
+            }
+        });
+
     }
+
+    public <T> void syncRequest(final Call<T> requestCall,final IResonseListener<T> resonseListener){
+
+        final FailureMessage failureMessage = new FailureMessage();
+        if (resonseListener == null) {
+            return;
+        }
+
+        Call<T> call;
+        if (requestCall.isExecuted()) {
+            call = requestCall.clone();
+        } else {
+            call = requestCall;
+        }
+
+        try{
+            Response<T> response = call.execute();
+            int resultCode = response.code();
+            if (response.isSuccessful()) {
+                T result = response.body();
+                if (result == null) {
+                    failureMessage.clear();
+                    failureMessage.setResultCode(resultCode);
+                    failureMessage.setFailureMessage("body为空");
+                    resonseListener.onFailure(failureMessage.toString());
+                }else {
+                    resonseListener.onSucess(result);
+                }
+            }else {
+                failureMessage.clear();
+                failureMessage.setResultCode(resultCode);
+                failureMessage.setFailureMessage(resultCode + "错误");
+                resonseListener.onFailure(failureMessage.toString());
+            }
+        } catch (IOException e) {
+            failureMessage.clear();
+            failureMessage.setResultCode(-1);
+            failureMessage.setFailureMessage(e.getMessage());
+            resonseListener.onFailure(failureMessage.toString());
+        }
+
+    }
+
+
+
 
 }
