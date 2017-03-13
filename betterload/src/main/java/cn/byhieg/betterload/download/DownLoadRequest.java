cca89525e631d33000be00bc891c92f808168885
石琦峰 <byhieg@gmail.com>
2017-03-13 23:27:01 +0800

    添加DownLoadRequest的代码

--- betterload/src/main/java/cn/byhieg/betterload/download/DownLoadRequest.java
+++ betterload/src/main/java/cn/byhieg/betterload/download/DownLoadRequest.java
@@ -1,6 +1,9 @@
 package cn.byhieg.betterload.download;
 
+import java.util.Iterator;
 import java.util.List;
+
+import cn.byhieg.betterload.utils.FailureMessage;
 
 /**
  * Created by byhieg on 17/3/13.
@@ -12,13 +15,33 @@ public class DownLoadRequest {
     private DownLoadHandle downLoadHandle;
 
     private List<DownLoadEntity> list;
+    private FailureMessage failMessage;
 
-    public DownLoadRequest(List<DownLoadEntity> list){
+    private IDownLoadListener listener;
+
+    public DownLoadRequest(List<DownLoadEntity> list,IDownLoadListener listener){
         this.list = list;
+        this.listener = listener;
+        failMessage = new FailureMessage();
     }
 
 
     public void start(){
-        List<DownLoadEntity> queryList = 
+        List<DownLoadEntity> queryList = downLoadHandle.queryDownLoadInfo(list);
+        Iterator it = queryList.iterator();
+        long totalFileSize = 0;
+        long hasDownSize = 0;
+
+        while (it.hasNext()) {
+            final DownLoadEntity entry = (DownLoadEntity) it.next();
+            hasDownSize += entry.getDownedData();
+            if (entry.getTotal() == 0) {
+                failMessage.clear();
+                failMessage.setFailureMessage("文件读取失败");
+                failMessage.setResultCode(-1);
+                listener.onError(entry,failMessage);
+                return;
+            }
+        }
     }
 }
